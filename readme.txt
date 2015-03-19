e83662c015d580a4a123889d8baa673e78f5749e
eVendi.de <eVendi.de@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-19 11:52:55 +0000

    First upload
    git-svn-id: https://plugins.svn.wordpress.org/shopweltde-widget/tags/1.01@1115987 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,52 @@
+﻿=== shopwelt.de Widget ===
+Contributors: shopwelt.de
+Donate link: https://www.shopwelt.de/
+Tags: Preisvergleich, Preis, Price Comparison, Affiliate
+Requires at least: 3.0.1
+Tested up to: 4.1
+Stable tag: 1.01
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Der Preisvergleich für Deinen WordPressBlog - Price Comparison Extension for your WordPress Blog.
+
+== Description ==
+
+Das **shopwelt.de – WordPress Plugin** bietet  Ihnen ab sofort die Möglichkeit, auf Ihrer Webseite konkrete Preise von einzelnen Produkten neben Produkttests, Produktbeschreibungen oder Produktempfehlungen zu integrieren.Sofern Sie bislang gar keine Preisinformationen zu Produkten, unverbindliche Preisempfehlungen oder nur die Preise eines einzelnen Anbieters angezeigt haben, ist das shopwelt.de WordPress Plugin genau richtig für Sie. Integrieren Sie ganz einfach eine Art Mini-Preisvergleich direkt in Ihre Webseite und verdienen Sie mit jedem Shopklick einen **Werbekostenzuschuss** (Konditionen erfahren Sie bei der Registrierung).
+
+Angaben zu Preisen, Verfügbarkeit und Versandkosten aus z.Zt ca. über 70 Millionen Angeboten werden ständig aktuell gepflegt und stehen fast in Echtzeit zur Verfügung.
+
+= Ihre Vorteile: =
+* Vergütung, Sie, als Webseitenbetreiber, werden von uns an jedem Umsatz beteiligt, den wir durch einen Ihrer Nutzer generieren. Dabei werden auch Klicks vergütet, die nicht direkt in einen Online-Shop gehen, sondern erst zu shopwelt.de und anschließend in einen Online-Shop.
+* Mehrwert, die Besucher Ihrer Webseite bekommen zusätzliche Informationen zu den Produkten in Form von aktuellen und attraktiven Preise sowie der Möglichkeit, die Produkte direkt online zu erwerben, ohne erst auf anderen Webseiten nach Preisen suchen zu müssen.
+* Das shopwelt.de WordPress Plugin ist **kostenfrei!**
+
+Über den Beitrags-Editor (egal ob Visuell oder Text) wird einfach an der gewünschten Stelle das Widget mit eckigen Klammern (Shortcode) aktiviert/eingefügt `[evwidget=SUCHBEGRIFF]`. Das gewünschte Produkt wird einfach als Suchbegriff direkt ohne Leerzeichen oder Anführungszeichen nach dem „=“ angefügt.
+
+= WICHTIG! =
+
+Der Suchbegriff sollte so genau wie möglich angegeben werden, also z.B. „iPhone 6 64 GB“ und nicht einfach „iPhone“, oder „Apple“. Dies bewirkt nur ungenaue und unbefriedigende Ergebnisse. Über shopwelt.de Datenbank werden dann im fertig erstellten Beitrag automatisch die entsprechenden Angebote angezeigt. Dabei passt sich die Anzeige und Darstellung des shopwelt.de Widgets automatisch der verfügbaren Breite an.
+
+Das shopwelt.de Plugin ist kostenfrei und kann zeitlich unbegrenzt getestet werden. Auszahlungen von Provisionen jedoch sind ohne einen von shopwelt.de zugewiesenen PubTag und ohne vorherige Registrierung nicht möglich.
+
+== Installation ==
+
+1. Kopieren und entpacken Sie die Datei `evendide-widget.zip` in das Verzeichnis `/wp-content/plugins/`
+2. Aktivieren Sie das Plugin im WordPress Admin Bereich 'Plugins'
+3. Passen Sie die Werte nach Ihren Vorstellungen an, z.B. Anzahl der anzuzeigenden Angebote, Farbe, Schriftart, Schriftgröße, etc.
+
+== Frequently Asked Questions ==
+
+== Screenshots ==
+
+1. So kann das eVend.de Widget in Ihrem Blog aussehen.
+
+== Changelog ==
+
+= 1.01 =
+* Automatische Anpassung der Darstellung für Responsive Design
+
+= 1.0 =
+* Erste Version
+
+
---
+++ widget.php
@@ -0,0 +1,166 @@
+﻿<?php
+/*
+Plugin Name: shopwelt.de-Widget 
+Plugin URI: https://www.shopwelt.de/
+Description: Diese Plugin erm&ouml;glicht das Einbinden des shopwelt.de-Widget 
+Version: 1.01
+Author: eVendi GmbH & Co. KG
+Author URI: https://www.shopwelt.de/
+*/
+$ShopweltWidgetPluginVersion = '1.01';
+function addShopweltWidgetToPost($content='') {
+	try {
+		if(preg_match_all('/\[evwidget[^\]]+?\]/i', $content, $evWidInfo)) {
+			foreach ($evWidInfo[0] as $item) {
+				preg_match_all('/evwidget=([^\]]*)\]/i', $item, $evkeyw);
+				$evdiv = '<div rel="evwidget" evwig="' . $evkeyw[1][0] . '" style="width:100%;"></div>';
+				$content = str_replace($item, $evdiv, $content);
+				$evkeyw = null;
+			}
+		} 
+	} catch (Exception $e) {
+	}
+	return $content;
+}
+
+function addShopweltWidgetFooter() {
+	$Opt = ShopweltLoadOptions();  
+
+    echo '<script type="text/javascript">';
+	echo 'var _eVWig = { pubtag: \'' . $Opt['PubTag'] . 
+			'\', type: \'diroffer\', font: \'' . $Opt['Font'] . 
+			'\', fontsize: ' . $Opt['FSize'] . ', linkcolor: \'' . $Opt['CLnk'] . '\', spacerline: \'' . $Opt['CSpl'] . 
+			'\', backcolor: \'' . $Opt['CBg'] . '\', number: ' . $Opt['OffCnt'] . ' };';
+	echo '</script><script type="text/javascript" src="//data.shopwelt.de/v101/liveprice.js"></script><script type="text/javascript">';
+	echo 'var _eVWiget = new _eVWigetLoader();_eVWiget.load();</script>';
+}
+
+function ShopweltLoadOptions() {
+	return Array (
+		'PubTag' => get_option('_ShopweltWidgetOption_PubTag', 'evwpwidem-de'),
+		'Font' => get_option('_ShopweltWidgetOption_Font', 'Arial, Verdana'),
+		'FSize' => get_option('_ShopweltWidgetOption_FontSize', '14'),
+		'CSpl' => get_option('_ShopweltWidgetOption_CSpLine', '#CCCCCC'),
+		'CLnk' => get_option('_ShopweltWidgetOption_CLnk', '#000000'),
+		'CBg' => get_option('_ShopweltWidgetOption_CBg', '#FFFFFF'),
+		'OffCnt' => get_option('_ShopweltWidgetOption_OfferCnt', '7')
+	);  
+}
+
+function ShopweltAdminOptions() {
+	if(!current_user_can('manage_options')){wp_die( __('You do not have sufficient permissions to access this page.') );}
+	$hidden_field_name='ShopweltWigUpdate';
+	
+	$Opt = ShopweltLoadOptions();
+	
+	if(isset($_POST[$hidden_field_name]) && $_POST[$hidden_field_name]=='Y'){
+		$Opt['PubTag'] = $_POST['evPubTag'];
+		$Opt['Font'] = $_POST['evFont'];
+		$Opt['FSize'] = $_POST['evFSize'];
+		$Opt['CSpl'] = $_POST['evCSpl'];
+		$Opt['CBg'] = $_POST['evCBg'];
+		$Opt['CLnk'] = $_POST['evCLnk'];
+		
+		$Opt['OffCnt'] = $_POST['evOffCnt'];
+		update_option('_ShopweltWidgetOption_PubTag', $Opt['PubTag']);
+		update_option('_ShopweltWidgetOption_Font', $Opt['Font']);
+		update_option('_ShopweltWidgetOption_FontSize', $Opt['FSize']);
+		update_option('_ShopweltWidgetOption_CSpLine', $Opt['CSpl']);
+		update_option('_ShopweltWidgetOption_CBg', $Opt['CBg']);
+		update_option('_ShopweltWidgetOption_CLnk', $Opt['CLnk']);
+		update_option('_ShopweltWidgetOption_OfferCnt', $Opt['OffCnt']);
+		?><div class="updated"><p><strong>Einstellungen gespeichert</strong></p></div><?php }
+	?><div class="wrap" id="poststuff">
+	<style>
+		.evcolorboxdefault {
+			display:inline-block; width:20px; margin-left:3px; border: 1px solid #CCC; height:20px; vertical-align: top;
+		}
+	</style>
+	<h2>shopwelt.de WordPress Plugin (Version <?php  echo $GLOBALS['ShopweltWidgetPluginVersion']; ?>)</h2>
+		<?php 
+		if(strlen($Opt['PubTag']) == 0) { 
+			?><p style="color:#990000;">Um dieses Plugin nutzen zu k&ouml;nnen muss der "PubTag" ausgef&uuml;llt sein.</p><?php } 
+		?>
+		<div class="postbox">
+		<h3 class="hndle"><span>Grundeinstellungen</span></h3>
+		<div class="inside">
+		<form name="form1" method="post" action="" style="margin:0px">
+			<input type="hidden" name="<?php echo $hidden_field_name; ?>" value="Y">
+			<p><table cellpadding="0" cellspacing="5" border="0" style="table-layout:fixed;">
+				<tr>
+					<td width="220"><b>Partner Tag:</b></td>
+					<td width="220"><input type="text" name="evPubTag" value="<?php echo $Opt['PubTag']; ?>" size="30"></td>
+					<td>Der „PublisherTag“ (PubTag) ist für die Abrechnung und die Zuordnung der generierten Klicks/Weiterleitungen notwendig. Diesen erhalten Sie <u>nach</u> erfolgreicher Registrierung bei shopwelt.de. Für die Dauer des kostenfreien Testzeitraums ist eine Demo PubTag voreingestellt. Die Dauer des Tests ist nicht begrenzt. Auszahlungen von Provisionen jedoch sind ohne einen von shopwelt.de zugewiesenen PubTag und ohne vorherige Registrierung nicht möglich.</td>
+				</tr>
+				<tr>
+					<td><b>Schriftart:</b></td>
+					<td><input type="text" name="evFont" value="<?php echo $Opt['Font']; ?>" size="20"></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td><b>Schriftgr&ouml;&szlig;e:</b></td>
+					<td><input type="text" name="evFSize" value="<?php echo $Opt['FSize']; ?>" size="5"> px</td>
+					<td></td>
+				</tr>
+				<tr>
+					<td><b>Farbe der Trennlinie:</b></td>
+					<td><input type="text" name="evCSpl" value="<?php echo $Opt['CSpl']; ?>" size="15"><span class="evcolorboxdefault" style="background-color:<?php echo $Opt['CSpl']; ?>"></span></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td><b>Hintergrundfarbe:</b></td>
+					<td><input type="text" name="evCBg" value="<?php echo $Opt['CBg']; ?>" size="15"><span class="evcolorboxdefault" style="background-color:<?php echo $Opt['CBg']; ?>"></span></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td><b>Linkfarbe:</b></td>
+					<td><input type="text" name="evCLnk" value="<?php echo $Opt['CLnk']; ?>" size="15"><span class="evcolorboxdefault" style="background-color:<?php echo $Opt['CLnk']; ?>"></span></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td><b>Maximale Anzahl der Angebote:</b></td>
+					<td><input type="text" name="evOffCnt" value="<?php echo $Opt['OffCnt']; ?>" size="5"></td>
+					<td>Hier wird fest definiert wie viele Angebote das Widget ausgegeben soll. <br />
+					Sollten keine Angebote vorhanden sein, wird das Widget nicht angezeigt.</td>
+				</tr>
+				
+				<tr>
+					<td></td>
+					<td><input type="submit" name="Submit" class="button-primary" value="Speichern" /></td>
+					<td></td>
+				</tr>
+			</table></p>
+		</form>
+		</div></div>
+			<div class="postbox">
+			<h3 class="hndle"><span>Informationen zum Plugin</span></h3>
+			<div class="inside">
+			<p>Das shopwelt.de – WordPress Plugin ermöglicht die einfache Integration eines Preisvergleiches von Produkten an jeder gewünschten Stelle eines Posts Ihres WordPress Blogs. Angaben zu Preisen, Verfügbarkeit und Versandkosten aus z.Zt ca. über 60 Millionen Angeboten werden ständig aktuell gepflegt und stehen fast in Echtzeit zur Verfügung.</p>
+
+			<p><b>Einbinden des Widgets</b></p>
+			
+      <p>Über den Beitrags-Editor (egal ob Visuell oder Text) wird einfach an der gewünschten Stelle das Widget mit eckigen Klammern (Shortcode) aktiviert/eingefügt: <br><br><b><em>[evwidget=SUCHBEGRIFF]</em></b> <br><br> Das gewünschte Produkt wird einfach als Suchbegriff direkt ohne Leerzeichen oder Anführungszeichen nach dem „=“ angefügt.</p>
+      <p><b>WICHTIG!</b></p>
+      <p>Der Suchbegriff sollte so genau wie möglich angegeben werden, also z.B. „iPhone 6 64 GB“ und nicht einfach „iPhone“, oder „Apple“. Dies bewirkt nur ungenaue und unbefriedigende Ergebnisse.</p>
+      <p>Über shopwelt.de Datenbank werden dann im fertig erstellten Beitrag automatisch die entsprechenden Angebote angezeigt. Dabei passt sich die Anzeige und Darstellung des shopwelt.de Widgets automatisch der verfügbaren Breite an.</p>
+			
+			<p>Beispiel: [evwidget=iPhone 6 64GB]<br /><br /><img src="<?php echo plugins_url( 'beispiel.png', __FILE__ ) ?>" /></p>
+			
+			</div></div>
+	</div><?php 
+}
+
+function addShopweltPagePluginSettings($links) {
+	$mylinks = array('<a href="' . admin_url( 'options-general.php?page=' . plugin_basename(__FILE__) ) . '">'.__('Settings').'</a>');
+	return array_merge( $links, $mylinks );
+}
+
+function addShopweltToAdminMen() { 
+	add_options_page('shopwelt.de-Widget', 'shopwelt.de-Widget', 8, __FILE__, 'ShopweltAdminOptions');
+}
+
+add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'addShopweltPagePluginSettings', 10, 2);
+add_action('wp_footer', 'addShopweltWidgetFooter');
+add_filter('the_content', 'addShopweltWidgetToPost');
+add_action('admin_menu', 'addShopweltToAdminMen');
+?>
